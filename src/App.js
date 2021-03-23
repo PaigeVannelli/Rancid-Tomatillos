d50f8ab923a5f1de614da21286d866ddf79fa987
Paige Vannelli <paigevannelli@gmail.com>
2021-03-23 16:30:09 -0700

    Create nav bar and movie logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import movieData from './movieData.js';
 import React, {Component} from 'react';
 import Movies from './Movies/Movies.js'
+import './App.css'
+import logo from './logo.svg'
 
 class App extends Component {
   constructor() {
@@ -12,7 +14,11 @@ class App extends Component {
 
   render() {
     return (
-      <main>
+      <main className='main-page'>
+        <nav className='nav'>
+          <button className='main-logo'><img src={logo} className='movie-reel-logo'/>Rancid<br>
+          </br>Tomatillos</button>
+        </nav>
         <Movies movieData={this.state.movieData}/>
       </main>
     );
